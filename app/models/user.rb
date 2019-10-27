3833a5e1415b782682e50cb940b25495aebb71ce
daiyada <shunsuke07l6@gmail.com>
2019-10-27 11:39:27 +0900

    フォロー機能実装のための必要ファイルを揃えました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   
   has_one :icon, dependent: :destroy
   has_many :likes
+  has_many :relationships
+  has_many :followees, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_072945) do
+ActiveRecord::Schema.define(version: 2019_10_26_094041) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category", null: false
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(version: 2019_10_25_072945) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
@@ -84,6 +94,8 @@ ActiveRecord::Schema.define(version: 2019_10_25_072945) do
   add_foreign_key "images", "tweets"
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "tweets", "categories"
   add_foreign_key "tweets", "users"
 end
