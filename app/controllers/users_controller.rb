d02c39f9321a6436a521d2ae428d08445ce2de3b
daiyada <shunsuke07l6@gmail.com>
2019-10-31 18:49:50 +0900

    読書メーター作成のためのファイルを準備しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
   def index
   end
+
+  def meter
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
   resources :tweets, only: [:index, :create]
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+    member do
+      get "meter" => "users#meter"
+    end
+  end
 end
