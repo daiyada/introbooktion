d65a628db505171506d1c08f40adc024e6537a64
daiyada <shunsuke07l6@gmail.com>
2019-11-01 16:26:10 +0900

    部分テンプレート化しました。

--- app/assets/javascripts/profile_switch_tab.js
+++ app/assets/javascripts/profile_switch_tab.js
@@ -1,7 +1,7 @@
 $(document).on("turbolinks:load",function(){
   $(".tweet-list__content__tablist__tab").on("click", function(){
     $(".tweet-list__content__tablist__tab.is_active").removeClass("is_active");
-		$(this).addClass('is_active');
+    $(this).addClass('is_active');
     $(".tweet-list__content__tabcontent.is_shown").removeClass("is_shown");
     var index = $(this).index();
     $(".tweet-list__content__tabcontent").eq(index).addClass("is_shown");
